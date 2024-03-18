32cb8ef63ddcf1244664d9b975ec3c0146cff7ab
tejas1611 <tejasgoel01@hotmail.com>
2024-03-18 12:57:51 +0400

    market data: snapshot synthesizer

---
+++ exchange/market_data/snapshot_synthesizer.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "common/mcast_socket.hpp"
+#include "common/logger.hpp"
+#include "common/mem_pool.hpp"
+#include "common/macros.hpp"
+#include "market_data/market_update.hpp"
+#include "matching_engine/me_order.hpp"
+
+namespace Exchange
+{
+class SnapshotSynthesizer {
+private:
+    PubMarketUpdateLFQueue* snapshot_md_updates_ = nullptr;
+    Common::MCastSocket snapshot_updates_socket_;
+    
+    volatile bool running_ = false;
+
+    std::array<std::array<MEMarketUpdate*, ME_MAX_ORDER_IDS>, ME_MAX_TICKERS> ticker_orders_;
+    size_t last_inc_seq_num_ = 0;
+    Nanos last_snapshot_time_ = 0;
+
+    Common::MemPool<MEMarketUpdate> order_pool_;
+
+    Logger logger_;
+    std::string time_str_;
+
+    PubMarketUpdate market_update_;
+
+public:
+    SnapshotSynthesizer(PubMarketUpdateLFQueue* snapshot_md_updates, const std::string &iface, const std::string &snapshot_ip, int snapshot_port);
+    ~SnapshotSynthesizer();
+
+    void start();
+    void stop();
+    void run();
+    void addToSnapshot(const PubMarketUpdate* market_update);
+    void publishSnapshot();
+
+    SnapshotSynthesizer() = delete;
+    SnapshotSynthesizer(const SnapshotSynthesizer&) = delete;
+    SnapshotSynthesizer(const SnapshotSynthesizer&&) = delete;
+    SnapshotSynthesizer& operator=(const SnapshotSynthesizer&) = delete;
+    SnapshotSynthesizer& operator=(const SnapshotSynthesizer&&) = delete;
+};
+}
